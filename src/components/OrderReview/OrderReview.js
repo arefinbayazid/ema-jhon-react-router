03e0628f1cf83dd38ac16d3209718ac711f2af8f
arefinbayazid <arefinbayazid@gmail.com>
2021-12-20 09:15:28 +0600

    inventory and order review page edited

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import useCart from '../Hooks/UserProduct/useCart';
+import useProduct from '../Hooks/UserProduct/useProduct';
 
 const OrderReview = () => {
+    const [products] = useProduct();
+    const [cart] = useCart(products);
     return (
         <div>
+            <h1>{products.length}</h1>
+            <h1>{cart.length}</h1>
             <h2>This is Order Review</h2>
         </div>
     );
